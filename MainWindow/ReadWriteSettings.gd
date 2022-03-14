c95f34a6abb5825881c1e2a5ef9f143cad42f0f4
Wendfrey <toni.cano.redondo@gmail.com>
2022-03-14 20:05:04 +0100

    Remove settings file asserts

--- MainWindow/ReadWriteSettings.gd
+++ MainWindow/ReadWriteSettings.gd
@@ -7,17 +7,22 @@ func _ready():
 
 func read_settings_file() -> Dictionary:
 	var settings_file: File = File.new()
-	assert(settings_file.open(ProjectSettings.get_setting("global/conf_filename"), File.READ) == OK)
+	if settings_file.open(ProjectSettings.get_setting("global/conf_filename"), File.READ):
+		print_debug("Cannot read file")
+		return {}
 	
 	var jsonResult: JSONParseResult = JSON.parse(settings_file.get_as_text())
 	settings_file.close()
-	assert(jsonResult.error == OK, "Error {eCode}: {msg} en la linea {ln}".format({code=jsonResult.error, msg = jsonResult.error_string, ln = jsonResult.error_line}))
+	if(jsonResult.error):
+		print_debug("Cannot parse file")
+		return {}
 	
 	return jsonResult.result
 
 func write_settings_file():
 	var settings_file: File = File.new()
-	assert(settings_file.open(ProjectSettings.get_setting("global/conf_filename"), File.WRITE) == OK)
+	if(settings_file.open(ProjectSettings.get_setting("global/conf_filename"), File.WRITE)):
+		print_debug("Cannot find file")
 	settings_file.store_string(JSON.print(current_settings, "\t"))
 	settings_file.flush()
 	settings_file.close()
